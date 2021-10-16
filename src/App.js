f7a57e4199dc04040ca505bc481a00bcb2de6830
Abimaelr <abimaelr.albu@gmail.com>
2021-10-16 18:51:12 -0300

    Configurando o contextAPI

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import { Context } from "./Context/Context";
+import Home from "./Screens/Home";
+import User from "./Screens/User";
 
 function App() {
   return (
     <div className="App">
-     
+      <Context.Provider>
+        <Router>
+          <Switch>
+            <Route path="/" component={ Home }/>
+            <Route path="/user/:position" component={ User }/>
+          </Switch>
+        </Router>
+      </Context.Provider>
     </div>
   );
 }
