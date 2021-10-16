aeb1aa4bfbfd84c3d3cd1688d020fbf34eec513f
Abimaelr <abimaelr.albu@gmail.com>
2021-10-16 19:43:16 -0300

    Alimentando os estados

--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -1,24 +1,35 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState} from 'react';
 import { Container } from 'react-bootstrap';
 import Context from '../Context/Context';
 
 function Home() {
-    const { users, setUsers } = useContext(Context)
+    const { users, setUsers } = useContext(Context);
+
+    const [gender, setGender] = useState('');
+    const [nacionality, setNacionality] = useState('');
+    const [amount, setAmount] = useState('');
+
+    const gerar = (e) => {
+        e.preventDefault();
+        console.log(amount)
+    }
+
     return (
         <div>
             <Container>
                 <form>
                     <label htmlFor="qnt">Quantas Pessoas?</label>
-                    <input type="number" name="qnt" id="qnt" />
+                    <input min="10" defaultValue={10} type="number" name="qnt" id="qnt"  onChange={({ target }) => setAmount(target.value)}/>
                     
                     <label htmlFor="gender">Gênero</label>
-                    <select name="gender" id="gender">
+                    <select name="gender" id="gender" onChange={({ target }) => setGender(target.value)}>
+                        <option value="">Todos</option>
                         <option value="male">Homem</option>
                         <option value="mulher">Mulher</option>
                     </select>
-
                     <label htmlFor="nacionality">Nacionalidade</label>
-                    <select name="nacionality" id="nacionality">
+                    <select name="nacionality" id="nacionality" onChange={ ({ target }) => setNacionality(target.value)}>
+                        <option value="">Todas</option>
                         <option value="au">Australia</option>
                         <option value="br">Brasil</option>
                         <option value="ca">Canadá</option>
@@ -37,6 +48,7 @@ function Home() {
                         <option value="tr">Turquia</option>
                         <option value="us">Estados Unidos</option>
                     </select>
+                    <button type="submit" onClick={ (e) => gerar(e) }>Gerar</button>
                 </form>
             </Container>
         </div>
