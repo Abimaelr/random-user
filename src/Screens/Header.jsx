95b702f8eb6e316c58fa48d8b7349ae36fff27c6
Abimaelr <abimaelr.albu@gmail.com>
2021-10-16 19:46:29 -0300

    Adicionando o Axios

---
+++ src/Screens/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div>
+            <header>
+                <h1>Gerador de Pessoas</h1>
+            </header>
+        </div>
+    )
+}
+
+export default Header
