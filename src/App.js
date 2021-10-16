24a7bdfb4468f8fb0b5c169cd2148f9947e97bb8
Abimaelr <abimaelr.albu@gmail.com>
2021-10-16 19:12:07 -0300

    Finalização da configuração dos contextos

--- src/App.js
+++ src/App.js
@@ -4,21 +4,21 @@ import {
   Switch,
   Route,
 } from "react-router-dom";
-import { Context } from "./Context/Context";
+import Provider from "./Context/Provider";
 import Home from "./Screens/Home";
 import User from "./Screens/User";
 
 function App() {
   return (
     <div className="App">
-      <Context.Provider>
+      <Provider>
         <Router>
           <Switch>
             <Route path="/" component={ Home }/>
             <Route path="/user/:position" component={ User }/>
           </Switch>
         </Router>
-      </Context.Provider>
+      </Provider>
     </div>
   );
 }
