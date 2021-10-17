5bfe213950405433897e81f730f75e46fba17fb9
Abimaelr <abimaelr.albu@gmail.com>
2021-10-16 21:17:34 -0300

    Comecando a implementar a tela de informacoes

--- src/Components/User.jsx
+++ src/Components/User.jsx
@@ -6,9 +6,15 @@ function User() {
     const { id } = useParams();
 
     const { users } = useContext(Context)
+
+    const user = users[id];
+    const { picture:{large}, dob, name:{ first, last, title}, email } = user;
+    console.log(user)
     return (
         <div>
-            { users[id].nat }
+            <h2>{ `${title}. ${first} ${last} `}</h2>
+            <img src={large} alt={ first} />
+            <a href={`mailto:${ email }`}>{ email }</a>
         </div>
     )
 }
